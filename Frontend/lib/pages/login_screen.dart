59b2dff7f75ede8d9f95ca5472004793d120d073
KGMalith <it18258622@my.sliit.lk>
2022-04-08 01:57:30 +0530

    question generation view updated

--- Frontend/lib/pages/login_screen.dart
+++ Frontend/lib/pages/login_screen.dart
@@ -140,11 +140,9 @@ class _LoginScreenState extends State<LoginScreen> {
                         setState(() {
                           isAPIcallProcess = false;
                         });
-                        await _storage.write(
-                            key: "Bearer_Token",
-                            value: jsonEncode(respond['token']));
                         SharedPreferences pref =
                             await SharedPreferences.getInstance();
+                        await pref.setString('Bearer_Token', respond['token']);
                         await pref.setString('user_name', respond['user_name']);
                         await pref.setString(
                             'user_email', respond['user_email']);
